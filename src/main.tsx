d3bbf07e92b976b8ebc9bafe40254e0fe3f4f04f
Lucas Guilhot <143024831+lucas04022002@users.noreply.github.com>
2026-03-30 10:12:05 +0200

    ajt article

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import App from "./App";
 
 import Home from "./page/Home";
 import About from "./page/About";
+import Article from "./page/Articles";
 
 // router creation
 
@@ -21,6 +22,10 @@ const router = createBrowserRouter([
 			{
 				path: "/about",
 				element: <About />,
+			},
+			{
+				path: "/articles/:id",
+				element: <Article />,
 			},
 		],
 	},
