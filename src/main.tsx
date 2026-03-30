6fbd5f489cf890b785c9625a829621571591a0ea
Lucas Guilhot <143024831+lucas04022002@users.noreply.github.com>
2026-03-30 10:06:07 +0200

    router page

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,35 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from "./App";
+
+// page components
+
+import Home from "./page/Home";
+import About from "./page/About";
+
+// router creation
+
+const router = createBrowserRouter([
+	{
+		element: <App />,
+		children: [
+			{
+				path: "/",
+				element: <Home />,
+			},
+			{
+				path: "/about",
+				element: <About />,
+			},
+		],
+	},
+]);
+
+// rendering
+
+const rootElement = document.getElementById("root");
+
+if (rootElement != null) {
+	ReactDOM.createRoot(rootElement).render(<RouterProvider router={router} />);
+}
